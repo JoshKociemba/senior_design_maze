23ce52f34f8ae4d01d7cfb487210824177748399
Tony Twyman <nw2056@yahoo.com>
2015-03-11 15:58:35 -0700

    Added asset folders, changed to maze.js
    Maze.js now plays audio and the rectangle has been replaced with a
    movable object with an animation.

--- maze.js
+++ maze.js
@@ -1,18 +1,75 @@
 $( document ).ready(function(){	
 	var canvas = document.getElementById("maze-canvas");
+	var sprites = [0, 0, 0, 0, 0, 0, 0];
+	var loaded = false;
 	var context = canvas.getContext("2d");
+	var imagePaths = ["images/spr_glasses_shadow_1.png","images/spr_glasses_shadow_2.png", "images/spr_glasses_shadow_3.png", "images/spr_glasses_shadow_4.png", "images/spr_glasses_shadow_5.png", "images/spr_glasses_shadow_6.png", "images/spr_glasses_shadow_7.png",];
 	var currRectX = 67;
 	var currRectY = 3;
 	var mazeWidth = 250;
 	var mazeHeight = 250;
+	var time = true;
+	var frameCount = 0;
 	var intervalVar;
-		
+	//music clip from www.FoolBoyMedia.co.uk
+	var gameOver = new Audio('sounds/game_over.mp3');
+	var bgs = new Audio('sounds/forest_song.wav');
+	bgs.volume = .7;
+	bgs.play();
+	var index = 0
+	var atomSpr = new Image();
+	var sprReady = false
+	atomSpr.onload = function () {sprReady = true;};
+	atomSpr.src = "images/spr_glasses_shadow_2.png";
+	var atom = { speed: 256};
+
+	// Handle keyboard controls   
+	var keysDown = {};
+
+	addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+	}, false);
+
+	addEventListener("keyup", function (e) {
+		delete keysDown[e.keyCode];
+	}, false);
+
+	loadImages(imagePaths)
+	
+	function loadImages(paths){
+  		paths.forEach(function(path){
+    		var img = new Image;
+    		img.onload = function(){
+      			for(x = 0; x < paths.length; x++){
+      				var sorc = img.src.split('.')
+      				path = paths[x].split('.')
+					if(path[0][path[0].length-1] == sorc[0][sorc[0].length-1]){
+						sprites[x] = img
+					}
+      			}
+      			if (sprites.length==paths.length) loaded = true;
+      		}
+      		img.src = path;
+      	});
+    }
+    
+	function reset() {
+		atom.x = 67;
+		atom.y = 3;
+	}
+
+	function render(bool, index){
+		if(bool){
+			context.drawImage(sprites[index], atom.x, atom.y, 12, 12);
+		}
+	}
+	
 	function drawMazeAndRectangle(rectX, rectY) {
 	    makeWhite(0, 0, canvas.width, canvas.height);
 	    var mazeImg = new Image();
 	    mazeImg.onload = function () {
 		context.drawImage(mazeImg, 0, 0);
-		drawRectangle(rectX, rectY, "#000001");
+		//drawRectangle(rectX, rectY, "#000001");
 		context.beginPath();
 		context.arc(89, 153, 7, 0, 2 * Math.PI, false);
 		context.closePath();
@@ -41,48 +98,71 @@ $( document ).ready(function(){
 	function moveRect(e) {
 	    var newX;
 	    var newY;
+	    /*var moving = false;
+	    var movingAllowed;
+	    if (38 in keysDown) { // Player holding up
+			newY -= atom.speed * modifier;
+			moving = true;
+		}
+		if (40 in keysDown) { // Player holding down
+			newY += atom.speed * modifier;
+			moving = true;
+		}
+		if (37 in keysDown) { // Player holding left
+			newX -= atom.speed * modifier;
+			moving = true;
+		}
+		if (39 in keysDown) { // Player holding right
+			newX += atom.speed * modifier;
+			moving = true;
+		}
+		if(moving){
+			atom.x = newX;
+			atom.y = newY;
+			}*/
 	    var movingAllowed;
 	    e = e || window.event;
 	    switch (e.keyCode) {
 		case 38:   // arrow up key
 		case 87: // W key
-		    newX = currRectX;
-		    newY = currRectY - 3;
+		    newX = atom.x;
+		    newY = atom.y - 3;
 		    break;
 		case 37: // arrow left key
 		case 65: // A key
-		    newX = currRectX - 3;
-		    newY = currRectY;
+		    newX = atom.x - 3;
+		    newY = atom.y;
 		    break;
 		case 40: // arrow down key
 		case 83: // S key
-		    newX = currRectX;
-		    newY = currRectY + 3;
+		    newX = atom.x;
+		    newY = atom.y + 3;
 		    break;
 		case 39: // arrow right key
 		case 68: // D key
-		    newX = currRectX + 3;
-		    newY = currRectY;
+		    newX = atom.x + 3;
+		    newY = atom.y;
 		    break;
 	    }
 	    movingAllowed = canMoveTo(newX, newY);
 	    if (movingAllowed === 1) {      // 1 means 'the rectangle can move'
-		drawRectangle(newX, newY, "#000001");
-		currRectX = newX;
-		currRectY = newY;
-	    }
-	    else if (movingAllowed === 2) { // 2 means 'the rectangle reached the end point'
-		clearInterval(intervalVar);
-		makeWhite(0, 0, canvas.width, canvas.height);
-		context.font = "40px Arial";
-		context.fillStyle = "blue";
-		context.textAlign = "center";
-		context.textBaseline = "middle";
-		context.fillText("Congratulations!", canvas.width / 2, canvas.height / 2);
-		window.removeEventListener("keydown", moveRect, true);
+	    	makeWhite(atom.x, atom.y, 12, 12);
+			atom.x = newX;
+			atom.y = newY;
+	   	}
+	   	else if (movingAllowed === 2) { // 2 means 'the rectangle reached the end point'
+			clearInterval(intervalVar);
+			makeWhite(0, 0, canvas.width, canvas.height);
+			context.font = "40px Arial";
+			context.fillStyle = "blue";
+			context.textAlign = "center";
+			context.textBaseline = "middle";
+			context.fillText("Congratulations!", canvas.width / 2, canvas.height / 2);
+			window.removeEventListener("keydown", moveRect, true);
 	    }
 	}
 	function canMoveTo(destX, destY) {
+		
 	    var imgData = context.getImageData(destX, destY, 12, 12);
 	    var data = imgData.data;
 	    var canMove = 1; // 1 means: the rectangle can move
@@ -108,6 +188,9 @@ $( document ).ready(function(){
 		intervalVar = setInterval(function () {
 			makeWhite(mazeWidth, 0, canvas.width - mazeWidth, canvas.height);
 			if (seconds === 0) {
+				time = false
+				gameOver.volume = .35
+				gameOver.play();
 				clearInterval(intervalVar);
 				window.removeEventListener("keydown", moveRect, true);
 				makeWhite(0, 0, canvas.width, canvas.height);
@@ -116,15 +199,16 @@ $( document ).ready(function(){
 				context.textAlign = "center";
 				context.textBaseline = "middle";
 				context.fillText("Carbon Time's Up!", canvas.width / 4, canvas.height / 4);
-				$("#next-url").css("visibility","visible");
 				return;
 			}
 			context.font = "20px Arial";
 			if (seconds <= 10 && seconds > 5) {
 				context.fillStyle = "orangered";
+				bgs.volume = .6
 			}
 			else if (seconds <= 5) {
 				context.fillStyle = "red";
+				bgs.volume = .4
 			}
 			else {
 				context.fillStyle = "green";
@@ -145,10 +229,42 @@ $( document ).ready(function(){
 		context.fill();
 	}
 
+	//Main game loop
+	function main(){
+		var now = Date.now();
+		//Used for animation
+		if((now - frameCount) >= 110){
+			frameCount = now;
+			if(index == 6){
+				index = 0;
+			}
+			else{
+				index += 1;
+			} 
+		}
+		//if there is still time we continue to render the atom
+		if(time){
+			render(loaded, index);
+			}
+		then = now;
+		requestAnimationFrame(main);
+	}
+
+	var w = window;
+	requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+
+	//draw the maze
 	drawMazeAndRectangle(67, 3);
+
+	
+	var then = Date.now();
+	frameCount = then
+	//set position of atom
+	reset();
+	//start the main game loop
+	main();
 	window.addEventListener("keydown", moveRect, true);
-	createTimer(20);
-	// while(1)
-		// if rectangle coordinates == circle coordinates
-			// go to next menu
+	createTimer(60);
+	
 });
