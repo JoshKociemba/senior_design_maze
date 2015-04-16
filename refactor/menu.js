e40ef064f89c98370d4ad7607472bf3425c8d3dc
hugheska <hugheska@onid.orst.edu>
2015-04-15 22:12:59 -0700

    Started the refactoring process by fixing menus in /refactor

---
+++ refactor/menu.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    function displayNextMenu() {
+		$(".menu").css("visibility","hidden");
+		x = (x === menu.length - 1) ? 0 : x + 1;
+		$(menu[x]).css("visibility","visible");
+	}
+	
+	function startTimer() {
+		setInterval(displayNextMenu, 3000);
+	}
+	
+	var menu = ["#main-menu", "#fuel-place-menu", "#place-atmo-menu", "#atmo-biomass-menu", "#end-menu"];
+	var x = 0;
+	
+	$(menu[x]).css("visibility","visible");
+	startTimer();
+});
